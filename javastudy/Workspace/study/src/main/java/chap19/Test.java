97ee227158e6413f7e62c9de28c062907c2b021e
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2021-11-04 22:28:33 +0900

    11월 4일

--- javastudy/Workspace/study/src/main/java/chap19/Test.java
+++ javastudy/Workspace/study/src/main/java/chap19/Test.java
@@ -1,6 +1,7 @@
 package chap19;
 
 public class Test {
+	static int d;
 	public static void main(String[] args) {
 		byte a = 0b1111;
 		System.out.println((int)a);
@@ -8,5 +9,14 @@ public class Test {
 		System.out.println(b);
 		int c = 017;
 		System.out.println(c);
+		System.out.println(d);
+		String abcd = "abcd";
+		System.out.println(abcd);
+		a(abcd);
+		System.out.println(abcd);
+	}
+	
+	static void a(String a) {
+		a.concat("efgh");
 	}
 }
