2453ce7529090c15bdafa84a4b1be5e9f7e90d2a
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2021-12-22 22:01:54 +0900

    12월 22일

--- app/Flutter/lib/main.dart
+++ app/Flutter/lib/main.dart
@@ -10,8 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(
-       child: Container(width: 50,height: 50,color: Colors.blue)
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Container(),
+        bottomNavigationBar: BottomAppBar(),
       )
     );
   }
