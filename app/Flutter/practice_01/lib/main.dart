47ec22ec988ef5bddfd604e757bd85bea313d02b
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2021-12-23 23:43:12 +0900

    12월 23일

---
+++ app/Flutter/practice_01/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          actions: [
+            Text("앱임")
+          ],
+        ),
+        body: Container(
+          child: Text("안녕"),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(Icons.phone),
+              Icon(Icons.message),
+              Icon(Icons.contact_page)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
