cdd7c5f93c563a57d669394c0e76139abb96fd21
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2021-12-28 14:53:12 +0900

    [12월 28일]플러터 숙제,커스텀 위젯

--- app/Flutter/test/lib/main.dart
+++ app/Flutter/test/lib/main.dart
@@ -10,25 +10,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('앱임'),),
-        body: Align(
-          alignment: Alignment.center,
-          child: Container(
-            width: double.infinity, height: 100,
-            decoration: BoxDecoration(
-              color: Colors.blue,
-            ),
-            margin: EdgeInsets.fromLTRB(20,20,20,20), padding: EdgeInsets.fromLTRB(20,20,20,20 ),
-            child: Row(
-              children: [
-                Text("이것은 앱입니다", style: TextStyle(color: Colors.red),),
-                ElevatedButton(child: Text('버튼'), onPressed: (){})
-              ],
-            ),
-          ),
-        )
-      )
+      home: ShopItem()
     );
   }
 }
+
+
+class ShopItem extends StatelessWidget {
+  const ShopItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child:Text('안녕'),
+    );
+  }
+}
+
--- app/Flutter/untitled/lib/main.dart
+++ app/Flutter/untitled/lib/main.dart
@@ -14,42 +14,31 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('금호동3가'),
         ),
-        body: SizedBox(
-          width: double.infinity, height: 110,
+        body: Container(
+          height: 150, padding: EdgeInsets.all(10),
           child: Row(
             children: [
-              Container(
-                  width: 130,
-                  height: 110,
-                  margin: EdgeInsets.all(4),
-                  child: Image.asset('dog.jpg')
-              ),
-              Column(
-                children: [
-                  SizedBox(
-                    width: 200,
-                    height: 90,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('캐논 DSLR 100D (단렌즈, 충전기 16기가 SD포함)', style: TextStyle(fontWeight: FontWeight.w400),),
-                        Text('성동구 행당동·끌올 10분 전', style: TextStyle(color: Colors.black26, fontWeight: FontWeight.w400),),
-                        Text('210,000원', style: TextStyle(fontWeight: FontWeight.w600),)
-                      ],
-                    ),
+              Flexible(child: Image.asset('dog.jpg'), flex: 3,),
+              Flexible(
+                flex: 7,
+                child: Container(
+                  margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('캐논 DSLR 100D (단렌즈, 충전기 16기가 SD포함)', style: TextStyle(fontWeight: FontWeight.w400),),
+                      Text('성동구 행당동·끌올 10분 전', style: TextStyle(color: Colors.black26, fontWeight: FontWeight.w400),),
+                      Text('210,000원', style: TextStyle(fontWeight: FontWeight.w600),),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Icon(Icons.favorite, color: Colors.black54,),
+                          Text('4', style: TextStyle(color: Colors.black54),)
+                        ],
+                      ),
+                    ],
                   ),
-                  SizedBox(
-                    width: 200,
-                    height: 20,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Icon(Icons.favorite_border, color: Colors.black54,),
-                        Text('4', style: TextStyle(color: Colors.black54),)
-                      ],
-                    ),
-                  )
-                ],
+                ),
               )
             ],
           ),
