9642ccfa06b2cd10c00534268a526a4b39fa8f0d
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-03-17 09:24:29 +0900

    [3월 17일] C# helloWorld

---
+++ C#/Hello/Hello/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+        }
+    }
+}
