952eb9c8b69ca4ede32eed7845a65d9645df6826
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-04-10 18:32:12 +0900

    [4월 10일] 스프링부트 시작

---
+++ SpringBoot(Kotlin)/Login/src/main/kotlin/com/login/login/LoginApplication.kt
@@ -0,0 +1,11 @@
+package com.login.login
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LoginApplication
+
+fun main(args: Array<String>) {
+	runApplication<LoginApplication>(*args)
+}
