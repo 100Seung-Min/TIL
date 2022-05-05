d9d26c4426b7fb11533fcd4dc6c2577f02a0ebee
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-05-05 21:58:56 +0900

    [5월 5일] 유니티 이동구현

---
+++ C#/유니티공부/First Project/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float maxSpeed;
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator anim;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //Stop 구현
+        if (Input.GetButtonUp("Horizontal"))
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+
+        // 방향 전환 구현
+        if (Input.GetButton("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+
+        // 이동 애니메이션 구현
+        if (rigid.velocity.normalized.x == 0)
+            anim.SetBool("isWalking", false);
+        else
+            anim.SetBool("isWalking", true);
+    }
+
+    void FixedUpdate()
+    {
+        // 이동구현
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        // maxSpeed 구현
+        if (rigid.velocity.x > maxSpeed)
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        else if (rigid.velocity.x < maxSpeed * (-1))
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+    }
+}
