71c1f9b62aa9ec0ba91170927b0d882ce32d0b86
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-06-13 22:52:20 +0900

    [6월 13일] ai 전공캠프 1일차

---
+++ Ai/전공캠프(2학년)/선형회귀.py
@@ -0,0 +1,15 @@
+from winreg import DeleteKey
+import numpy as np
+import matplotlib.pyplot as plt
+
+w, b = 3, -1
+x = np.random.normal(0, 1, size=(100, ))
+y = w*x + b
+noise = np.random.normal(0, 0.3, (100, ))
+y += noise
+# y = 1/(1+np.exp(-y)) # 시그모이드 함수로 변화
+
+fig, ax = plt.subplots(figsize=(10, 10))
+ax.scatter(x, y, alpha=0.5)
+ax.tick_params(labelsize=20)
+plt.show()
