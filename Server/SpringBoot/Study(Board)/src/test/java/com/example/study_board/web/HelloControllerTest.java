b5e15fc5d56d4cb6d35631f0febe2cc2572307d7
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-07-03 00:09:07 +0900

    [7월 3일] SpringBoot controller

---
+++ Server/SpringBoot/Study(Board)/src/test/java/com/example/study_board/web/HelloControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.study_board.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void return_hello() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
+    }
+}
