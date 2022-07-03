d9808ebd54077e2ff0a5e83bd109b9fb4b71fa90
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-07-03 17:15:34 +0900

    [7월 3일] SpringBoot PostsApi생성

---
+++ Server/SpringBoot/Study(Board)/src/main/java/com/example/study_board/service/PostsService.java
@@ -0,0 +1,19 @@
+package com.example.study_board.service;
+
+import com.example.study_board.domain.posts.PostsRepository;
+import com.example.study_board.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ Server/SpringBoot/Study(Board)/src/main/java/com/example/study_board/web/PostsApiController.java
@@ -0,0 +1,19 @@
+package com.example.study_board.web;
+
+import com.example.study_board.service.PostsService;
+import com.example.study_board.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+}
