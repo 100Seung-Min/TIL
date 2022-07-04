b4ecfd0905e00287fc732ea12459b719098865c6
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-07-04 20:19:46 +0900

    [7월 4일] SpringBoot BaseTimeEntity

--- Server/SpringBoot/Study(Board)/src/test/java/com/example/study_board/domain/posts/PostsRepositoryTest.java
+++ Server/SpringBoot/Study(Board)/src/test/java/com/example/study_board/domain/posts/PostsRepositoryTest.java
@@ -6,6 +6,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -33,5 +35,24 @@ public class PostsRepositoryTest {
         Posts posts = postsLists.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void set_BaseTimeEntity() {
+        LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>> createDate="+posts.getCreatedDate()+", modifiedDate="+posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
