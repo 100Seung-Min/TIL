34bbdd58b344aa1d6cb4a325b4a93e0b11934256
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-07-09 18:00:27 +0900

    [7월 9일] SpringBoot 어노테이션 기반 로그인

--- Server/SpringBoot/Study(Board)/src/main/java/com/example/study_board/web/IndexController.java
+++ Server/SpringBoot/Study(Board)/src/main/java/com/example/study_board/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.study_board.web;
 
+import com.example.study_board.config.auth.LoginUser;
 import com.example.study_board.config.auth.dto.SessionUser;
 import com.example.study_board.service.PostsService;
 import com.example.study_board.web.dto.PostsResponseDto;
@@ -19,9 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null) {
             model.addAttribute("name", user.getName());
         }
