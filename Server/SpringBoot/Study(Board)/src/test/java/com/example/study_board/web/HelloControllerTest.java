727cf1f299ed26746781e4bca2b1a616f88d231f
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-07-11 13:52:22 +0900

    [7월 11일] SpringBoot SecurityConfig 스캔 제외

--- Server/SpringBoot/Study(Board)/src/test/java/com/example/study_board/web/HelloControllerTest.java
+++ Server/SpringBoot/Study(Board)/src/test/java/com/example/study_board/web/HelloControllerTest.java
@@ -1,22 +1,32 @@
 package com.example.study_board.web;
 
+import com.example.study_board.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+                        classes = SecurityConfig.class)
+        })
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles = "USER")
     @Test
     public void return_hello() throws Exception {
         String hello = "hello";
@@ -26,6 +36,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void return_helloDto() throws Exception {
         String name = "hello";
