213bf56edf1603c053d546e3d94124b4dfa77282
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-09-15 16:01:43 +0900

    [9월 15일] App 앱프로그래밍 2일차

--- App/AppPrograming/FirstTime/app/src/main/java/com/example/firsttime/Test.kt
+++ App/AppPrograming/FirstTime/app/src/main/java/com/example/firsttime/Test.kt
@@ -1,11 +1,50 @@
 package com.example.firsttime
 
 fun main() {
-    var list = listOf(10, 20, 30)
-    println(
-        """
-            list size : ${list.size}
-            list data : ${list[0]}, ${list[1]}, ${list[2]}
-        """.trimIndent()
-    )
+//    var list = listOf(10, 20, 30)
+//    println(
+//        """
+//            list size : ${list.size}
+//            list data : ${list[0]}, ${list[1]}, ${list[2]}
+//        """.trimIndent()
+//    )
+
+//    var data: Any = 10
+//    val result = if (data > 10) {
+//        println("data > 10")
+//        false
+//    } else if (data in 1..10) {
+//        println("data >0 && data <= 10")
+//        true
+//    } else {
+//        println("data <= 0")
+//        false
+//    }
+//    println(result)
+//    when (data) {
+//        10 -> println("data is 10")
+//        20 -> println("data is 20")
+//        else -> {
+//            println("unknown")
+//        }
+//    }
+//
+//    when (data) {
+//        is String -> println("data is String")
+//        20, 30 -> println("data is 20 or 30")
+//        in 1..10 -> println("data is 1..10")
+//        else -> println("data is not valid")
+//    }
+
+//    var sum: Int = 0
+//    for (i in 1..10) {
+//        sum += i
+//    }
+//    println(sum)
+
+//    var data = arrayOf<Int>(10, 20, 30)
+//    for (i in data.indices) {
+//        print(data[i])
+//        if(i != data.size - 1) print(",")
+//    }
 }
