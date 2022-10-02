6161769ab0dffaff2d6db5ab5b4d21ccfc060e21
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-10-02 23:07:02 +0900

    [10월 2일] IOS 프로젝트 생성

---
+++ App/IOS/스위프트 기초/실습/Study/Study/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Study
+//
+//  Created by 백승민 on 2022/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
