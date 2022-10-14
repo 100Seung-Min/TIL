9b5cc475fbb998bac3001f839443b64bfc565219
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-10-14 09:32:28 +0900

    [10월 14일] IOS SwiftUI공부

---
+++ App/IOS/스위프트 기초/실습/StudySwiftUI/StudySwiftUI/StudySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySwiftUIApp.swift
+//  StudySwiftUI
+//
+//  Created by 백승민 on 2022/10/09.
+//
+
+import SwiftUI
+
+@main
+struct StudySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
