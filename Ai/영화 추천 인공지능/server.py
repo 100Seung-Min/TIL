db62411896f36ff6b7f49a057f5a4c5305c06170
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-04-04 19:22:04 +0900

    [4월 4일] AI 서버에 띄우기

---
+++ Ai/영화 추천 인공지능/server.py
@@ -0,0 +1,16 @@
+from flask import Flask, escape, request, jsonify
+import recommendAI
+
+# 서버 실행
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    movieName = request.args.get('movieName', 'The Dark Knight')
+    result = recommendAI.callAi(movieName)
+    if len(result) < 1 :
+        return 'Not Found', 404
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
